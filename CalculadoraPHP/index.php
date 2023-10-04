13b0a9ef062df0ceff7826127de47102f4b8d7f9
MarcosGalaxy <131073141+MarcosGalaxy@users.noreply.github.com>
2023-10-04 20:36:20 -0300

    Calculadora
    Site da Calculadora, utilizando HTML, CSS e PHP.

---
+++ CalculadoraPHP/index.php
@@ -0,0 +1,24 @@
+<?php
+function soma($num1,$num2){
+  $total = 0;
+  $total = $num1 + $num2;
+  return $total;
+}
+
+function subtracao($num1,$num2){
+  $total = 0;
+  $subtacao = $num1 - $num2;
+  return $subtacao;
+}
+
+function multiplicacao($num1,$num2){
+  $total = 0;
+  $total = $num1 * $num2;
+  return $total;
+}
+
+function divisao($num1,$num2){
+  $total = 0;
+  $total = $num1 * $num2;
+  return $total;
+}
